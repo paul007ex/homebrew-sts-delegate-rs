95cde939d5a154985a92d8f18991b0feb90d9e6f
Paul Volosen <43864272+paul007ex@users.noreply.github.com>
2026-06-11 20:38:34 -0500

    Simplify sts-cli formula platform selection

--- Formula/sts-cli.rb
+++ Formula/sts-cli.rb
@@ -4,26 +4,16 @@ class StsCli < Formula
   version "0.1.0-alpha.6"
   license "MIT"
 
-  on_macos do
-    on_arm do
-      url "https://github.com/paul007ex/sts-delegate-rs/releases/download/v0.1.0-alpha.6/sts-cli-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "ef8f886b7e74685eba47b6e22432d015dccf0782e9d61d5d56ba68e15490f2d5"
-    end
+  if OS.mac?
+    depends_on arch: :arm64
 
-    on_intel do
-      depends_on arch: :arm64
-    end
-  end
+    url "https://github.com/paul007ex/sts-delegate-rs/releases/download/v0.1.0-alpha.6/sts-cli-0.1.0-aarch64-apple-darwin.tar.gz"
+    sha256 "ef8f886b7e74685eba47b6e22432d015dccf0782e9d61d5d56ba68e15490f2d5"
+  elsif OS.linux?
+    depends_on arch: :x86_64
 
-  on_linux do
-    on_intel do
-      url "https://github.com/paul007ex/sts-delegate-rs/releases/download/v0.1.0-alpha.6/sts-cli-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "f65e667dc43ace824e571f8d8ebd2b74b15ce355955d5f98df9aab38a14e34cf"
-    end
-
-    on_arm do
-      depends_on arch: :x86_64
-    end
+    url "https://github.com/paul007ex/sts-delegate-rs/releases/download/v0.1.0-alpha.6/sts-cli-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "f65e667dc43ace824e571f8d8ebd2b74b15ce355955d5f98df9aab38a14e34cf"
   end
 
   def install
